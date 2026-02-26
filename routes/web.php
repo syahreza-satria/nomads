6d28dc35c2d238a7f175b088d89ddcbf721f8f93
syahreza-satria <satriaeza221@gmail.com>
2026-02-26 09:35:12 +0700

    transaction crud

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\GalleryController;
+use App\Http\Controllers\Admin\TransactionController;
 use App\Http\Controllers\Admin\TravelPackageController;
 use App\Http\Controllers\CheckoutController;
 use App\Http\Controllers\DetailController;
@@ -31,6 +32,7 @@ Route::prefix('admin')
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource('travel-package', TravelPackageController::class);
     Route::resource('gallery', GalleryController::class);
+    Route::resource('transaction', TransactionController::class);
 });
 
 Auth::routes(['verify' => true]);
